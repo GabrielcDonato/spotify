17c099f968127ba4036d028b3e953b7384f9e9bc
Gabriel Donato <gabrielcdonato@gmail.com>
2022-02-26 17:07:45 -0300

    Fix:(APP) put correct paths to core folder

--- lib/app_widget.dart
+++ lib/app_widget.dart
@@ -1,11 +1,10 @@
 import 'package:flutter/material.dart';
 
-import 'core/core..dart';
+import 'core/core.dart';
 
 class AppWidget extends StatelessWidget {
   const AppWidget({Key? key}) : super(key: key);
 
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
